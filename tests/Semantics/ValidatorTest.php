5f375315e767874bb2075b68937c31572298a4c0
Jan Svoboda <jansvoboda11@gmail.com>
2018-06-26 16:34:37 +0200

    Tweak validator tests

--- tests/Semantics/ValidatorTest.php
+++ tests/Semantics/ValidatorTest.php
@@ -12,72 +12,58 @@ use Svoboda\PsrRouter\Semantics\Validator;
 class ValidatorTest extends TestCase
 {
     /**
-     * @var Parser
+     * @doesNotPerformAssertions
      */
-    private $parser;
-
-    /**
-     * @var Validator
-     */
-    private $validator;
-
-    /**
-     * @inheritdoc
-     */
-    public function setUp()
-    {
-        $this->parser = new Parser();
-        $this->validator = new Validator();
-    }
-
     public function test_path_without_attributes()
     {
-        $path = $this->parser->parse("/users");
+        $path = (new Parser())->parse("/users");
 
-        $this->validator->validate($path);
-
-        self::assertTrue(true);
+        (new Validator())->validate($path);
     }
 
+    /**
+     * @doesNotPerformAssertions
+     */
     public function test_path_with_one_required_attribute()
     {
-        $path = $this->parser->parse("/users/{name}");
+        $path = (new Parser())->parse("/users/{name}");
 
-        $this->validator->validate($path);
-
-        self::assertTrue(true);
+        (new Validator())->validate($path);
     }
 
+    /**
+     * @doesNotPerformAssertions
+     */
     public function test_path_with_one_optional_attribute()
     {
-        $path = $this->parser->parse("/users[/{name}]");
+        $path = (new Parser())->parse("/users[/{name}]");
 
-        $this->validator->validate($path);
-
-        self::assertTrue(true);
+        (new Validator())->validate($path);
     }
 
+    /**
+     * @doesNotPerformAssertions
+     */
     public function test_path_with_two_attributes()
     {
-        $path = $this->parser->parse("/users/{id}/{name}");
+        $path = (new Parser())->parse("/users/{id}/{name}");
 
-        $this->validator->validate($path);
-
-        self::assertTrue(true);
+        (new Validator())->validate($path);
     }
 
+    /**
+     * @doesNotPerformAssertions
+     */
     public function test_path_with_required_and_optional_attribute()
     {
-        $path = $this->parser->parse("/users/{id}[/{name}]");
+        $path = (new Parser())->parse("/users/{id}[/{name}]");
 
-        $this->validator->validate($path);
-
-        self::assertTrue(true);
+        (new Validator())->validate($path);
     }
 
     public function test_path_with_two_required_attributes_of_same_name()
     {
-        $path = $this->parser->parse("/users/{id}/{id}");
+        $path = (new Parser())->parse("/users/{id}/{id}");
 
         $this->expectException(InvalidRoute::class);
         $this->expectExceptionMessage(<<<MESSAGE
@@ -86,12 +72,12 @@ Multiple attributes with name 'id':
 MESSAGE
         );
 
-        $this->validator->validate($path);
+        (new Validator())->validate($path);
     }
 
     public function test_path_with_required_and_optional_attribute_of_same_name()
     {
-        $path = $this->parser->parse("/users/{id}[/{id}]");
+        $path = (new Parser())->parse("/users/{id}[/{id}]");
 
         $this->expectException(InvalidRoute::class);
         $this->expectExceptionMessage(<<<MESSAGE
@@ -100,6 +86,6 @@ Multiple attributes with name 'id':
 MESSAGE
         );
 
-        $this->validator->validate($path);
+        (new Validator())->validate($path);
     }
 }
