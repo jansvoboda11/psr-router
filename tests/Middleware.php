7d6505fa1fb04f357c174cdc0fc785a11f5e2b37
Jan Svoboda <jansvoboda11@gmail.com>
2018-08-03 17:17:13 +0200

    Remove middleware dependency on Diactoros

--- tests/Middleware.php
+++ tests/Middleware.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace SvobodaTest\Router;
 
+use Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\TextResponse;
 
 class Middleware implements MiddlewareInterface
 {
@@ -21,6 +21,6 @@ class Middleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        return new TextResponse($this->name);
+        throw new Exception("Not implemented");
     }
 }
